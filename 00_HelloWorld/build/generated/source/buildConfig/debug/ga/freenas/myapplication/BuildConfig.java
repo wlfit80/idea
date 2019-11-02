54b24448d00b1d9f08539265000ce22a70bbfe3e
wlfit80 <wlfit80@gmail.com>
2019-11-02 15:01:12 +0800

    v1

---
+++ 00_HelloWorld/build/generated/source/buildConfig/debug/ga/freenas/myapplication/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package ga.freenas.myapplication;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "ga.freenas.myapplication";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
